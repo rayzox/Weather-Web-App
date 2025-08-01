6fe065dd07a91013b8bf77693d965160b6ece52e
rayzox <nizarkadiri70@gmail.com>
2025-08-01 17:56:58 +0100

    BaseModal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,25 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    fontFamily: {
-      Roboto: ["Roboto", "sans-serif"],
-    },
-    container: {
-        padding: "2rem",
-        center: true,
-    },
     extend: {
       colors: {
-        "weather-primary": "#000000",
+        "weather-primary": "#00668A",
         "weather-secondary": "#004E71",
       },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
     },
   },
   plugins: [],
